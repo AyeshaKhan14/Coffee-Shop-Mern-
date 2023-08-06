d44ad2c3e7dc46f99c6bc14ab2110ad3578df338
Ayeshakhan14 <ayeshatravels9@gmail.com>
2023-08-06 13:37:45 +0530

    function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { AllRoutes } from './Components/AllRoutes'
 
 function App() {
- 
 
   return (
     <>
